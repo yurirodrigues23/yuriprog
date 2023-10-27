aa99c85b60d81a7635bb5e7e9e072d93e88fcee1
yuri <yurirodribeiro@gmail.com>
2023-10-26 22:01:52 -0300

    carregamento aula1

---
+++ iscript.js
@@ -0,0 +1,44 @@
+//let pessoa1 = 'Yuri';//Essa variável pode ser reatribuída 
+//var pessoa2 = 'João';// Esse tipo de variável não é mais utilizado como boas práticas em java script
+//const pessoa3 = 'Lana';// A variável do tipo const, só pode ser atribuída uma única vez 
+
+//let mensagem = 'Olá, essa é a mensagem da minha variável';
+//console.log(mensagem);
+//mensagem = 'Essa é a nova mensagem da minha variável';
+//console.log(mensagem);
+
+//float: número que4 contém pontos ou vírgulas
+//int: números inteiros(não contém pontos ou vígulas)
+//bool: booleano; true(verdadeiro), false(falso)
+//string: do tipo texto, precisa ser colocado entre aspas duplas ou simples 
+
+//let num1 = 15;
+//let num2 = 20;
+
+//let soma = num1 + num2;
+//console.log(soma);
+
+//let multi = num1 * num2;
+console.log(multi);
+
+//let divisão = num1 / num2;
+console.log(divisão);
+
+//let sub = num1 - num2;
+//console.log(sub);
+
+//if: se
+//: senão
+
+// >: maior que 
+// <: menor que 
+// >=: maior ou igual
+// <=: menor ou igual
+// ===: igual
+
+
+if(num1 > 10){
+    console.log('O número 1 é maior do que 10');
+}else{
+    console.log('O número 1, não é maior do que 10');
+}
